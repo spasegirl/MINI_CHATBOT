94097488cc3b192ef2d6b4c41adc9c002bc2debb
Yousofy, Mohadeseh <mohadeseh.yousofy@lhind.dlh.de>
2025-01-19 13:54:49 +0100

    add confog for env variables

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuration settings
+UPLOAD_FOLDER = "static/upload"
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
--- modules/gpt_module.py
+++ modules/gpt_module.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template, request, jsonify
-from langchain_openai import AzureChatOpenAI
-import os
-from dotenv import load_dotenv
-from langchain_openai import AzureOpenAIEmbeddings
-
-load_dotenv()
-
-api_key = os.getenv("AZURE_OPENAI_API_KEY")
-api_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from config import AZURE_OPENAI_API_KEY, AZURE_OPENAI_ENDPOINT
 
 llm = AzureChatOpenAI(
     model_name="gpt-35-turbo-0613",
-    azure_endpoint=api_endpoint,
-    openai_api_key=api_key,
-    api_version="2024-05-01-preview",
-    request_timeout=60
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,
+    openai_api_key=AZURE_OPENAI_API_KEY,
+    api_version="2024-05-01-preview"
 )
 
-embedding_model = AzureOpenAIEmbeddings(api_key=api_key, 
-                                        azure_endpoint=api_endpoint, 
-                                        azure_deployment="text-embedding-ada-002", 
-                                        api_version="2024-05-01-preview")
+embedding_model = AzureOpenAIEmbeddings(
+    api_key=AZURE_OPENAI_API_KEY,
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,
+    azure_deployment="text-embedding-ada-002",
+    api_version="2024-05-01-preview"
+)
